5a33a959a78842726f551ca575916e09c1d334d3
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-10-14 15:51:22 -0300

    feature: stats strings

---
+++ Domain/Pokemon/Structs/Stats.cs
@@ -0,0 +1,11 @@
+namespace Pokemon.Domain.Pokemon.Structs;
+
+public struct Stats
+{
+    public static readonly string Attack = "Attack";
+    public static readonly string Defense = "Defense";
+    public static readonly string SpecialAttack = "Special Attack";
+    public static readonly string SpecialDefense = "Special Defense";
+    public static readonly string Speed = "Speed";
+    public static readonly string Hp = "HP";
+}
