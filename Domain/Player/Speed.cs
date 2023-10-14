1f061ac6acf85abab568735e407758840d2630a6
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-10-14 16:26:14 -0300

    doc: speed

--- Domain/Player/Speed.cs
+++ Domain/Player/Speed.cs
@@ -4,28 +4,35 @@ namespace Pokemon.Domain.Player;
 
 public struct Speed
 {
-    // ? variables ---------------------------------------------------------- //
-    private const ushort SpeedBoost = 2;
+    # region ---- properties ---------------------------------------------------
 
-    private ushort _value;
+    private const ushort SpeedBoost = 2;
     private ushort _baseSpeed;
 
-    // ? modifiers ---------------------------------------------------------- //
+    public ushort Value { get; private set; }
+
+    # endregion
+
+    # region ---- modifiers ----------------------------------------------------
+
     public void SpeedUp(ushort amount)
     {
-        _value *= amount;
+        Value *= amount;
         GD.Print(what: $"Speed boost of {amount}x");
     }
 
-    public void SpeedDown(ushort amount) => _value /= amount;
+    public void SpeedDown(ushort amount) => Value /= amount;
 
     public void ResetSpeed()
     {
-        _value = _baseSpeed;
+        Value = _baseSpeed;
         GD.Print(what: $"Speed reset back to {_baseSpeed}");
     }
 
-    // ? behaviors ---------------------------------------------------------- //
+    # endregion
+
+    # region ---- behaviors ----------------------------------------------------
+
     public void HandleTurbo()
     {
         if (Input.IsActionJustPressed("ui_speed_up"))
@@ -36,18 +43,25 @@ public struct Speed
         if (Input.IsActionJustReleased("ui_speed_up")) { ResetSpeed(); }
     }
 
-    // ? operators ---------------------------------------------------------- //
-    public static implicit operator ushort(Speed speed) => speed._value;
+    # endregion
 
-    public static implicit operator uint(Speed speed) => speed._value;
+    # region ---- implicit operators -------------------------------------------
+
+    public static implicit operator ushort(Speed speed) => speed.Value;
+
+    public static implicit operator uint(Speed speed) => speed.Value;
 
     public static implicit operator Speed(ushort value) => new()
     {
         _baseSpeed = value,
-        _value = value
+        Value = value
     };
 
-    // ? overrides ---------------------------------------------------------- //
+    # endregion
 
-    public override string ToString() => _value.ToString();
+    # region ---- to string ----------------------------------------------------
+
+    public override string ToString() => Value.ToString();
+
+    # endregion
 }
