eb17ec021e6ca2e2807b0b9cb74fd4ea239cf7dc
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-10-14 10:22:58 -0300

    player direction

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,30 +1,20 @@
 using Godot;
-using Pokemon.Valuable_Objects;
+using Pokemon.Domain.Player;
 
 namespace Pokemon.Scripts;
 
 public partial class Player : CharacterBody2D
 {
-	// [Export]
 	public Speed Speed = 50;
 
-	private const ushort SpeedBoost = 2;
+	private readonly Direction _direction = Vector2.Zero;
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Speed.HandleTurbo();
+		_direction.HandleDirection();
 
-		var direction = new Vector2
-			{
-				X = Input.GetActionStrength("ui_right") -
-				    Input.GetActionStrength("ui_left"),
-
-				Y = Input.GetActionStrength("ui_down") -
-				    Input.GetActionStrength("ui_up")
-			}
-			.Normalized();
-
-		var movement = direction * Speed * (float) delta;
+		var movement = (Vector2) _direction * Speed * (float) delta;
 
 		MoveAndCollide(movement);
 	}
