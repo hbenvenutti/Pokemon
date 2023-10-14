9341b3b114ff302f8d378269048b9f03025fe355
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-10-14 17:35:34 -0300

    fix: speed

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,10 +7,12 @@ public partial class Player : CharacterBody2D
 {
 	# region ---- properties ---------------------------------------------------
 
-	public Speed Speed { get; }= 50;
+	public Speed Speed { get; } = new();
 	public Direction Direction { get; } = Vector2.Zero;
 
 	# endregion
+
+	# region ---- fisics process -----------------------------------------------
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -18,7 +20,8 @@ public partial class Player : CharacterBody2D
 		Direction.HandleDirection();
 
 		var movement = (Vector2) Direction * Speed * (float) delta;
-
 		MoveAndCollide(movement);
 	}
+
+	# endregion
 }
