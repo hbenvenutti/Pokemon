9e9ab2a870dbf5918e385c29abb319a71e14813f
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-10-17 19:09:50 -0300

    input actions

--- Domain/Player/Structs/InputActions.cs
+++ Domain/Player/Structs/InputActions.cs
@@ -1,6 +1,12 @@
-namespace Pokemon.Scenes.Structs;
+namespace Pokemon.Domain.Player.Structs;
 
 public struct InputActions
 {
-    public const string Interact = "interaction";
+    public const string Interact = "ui_interact";
+    public const string SpeedUp = "ui_speed_up";
+
+    public const string MoveUp = "ui_up";
+    public const string MoveDown = "ui_down";
+    public const string MoveLeft = "ui_left";
+    public const string MoveRight = "ui_right";
 }
